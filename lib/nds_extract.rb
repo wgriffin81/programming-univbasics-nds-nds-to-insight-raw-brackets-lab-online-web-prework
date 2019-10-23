a9f63f30bbb9e45adc7235bbedbcdab55feece64
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-23 06:44:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,7 @@ while row_index < directors_database.length do
     while inner_index < inner_len do
       # Explanation!
       # vm[row][column][spinner]
-      # spinner is full of Hashes with keys :price and :name
+      # total titles by name - movies -- title for each director 
       grand_total += directors_database[row_index][column_index][inner_index][:title]
       inner_index += 1
     end
